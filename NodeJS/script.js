2213cca82c3035177370d9b55c5e1d6b96db5c27
Shubham Prakash <prakashshubham26@gmail.com>
2024-09-07 18:00:12 +0530

    Lectures NodeJs

---
+++ NodeJS/script.js
@@ -0,0 +1,29 @@
+// let n =5;
+
+// for(let i=0;i<n;i++){
+//     console.log("hello, ", i);
+// }
+// let args = process.argv;
+// for(let i=2;i<args.length;i++){
+//     console.log("Hello, " , args[i]);
+// }
+
+// const math = require("./math");
+
+// console.log(math.sum(2,2));
+// console.log(math.PI);
+// console.log(math.sub(2,1)); 
+
+// const info = require("./Fruits");
+
+// console.log(info); 
+
+
+import {sum,PI} from "./math.js";
+
+// console.log(sum(2,3));
+
+import {generate} from "random-words";
+
+console.log(generate());
+
